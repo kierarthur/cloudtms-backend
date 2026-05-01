1ff880786425f526f306dbece4852fa73433fefa
kierarthur <kier@arthur-rai.co.uk>
2026-05-01 18:06:11 +0100

    Update 08042026_1226_BANKING_PAY_NEW.sql

--- supabase/repeatable/08042026_1226_BANKING_PAY_NEW.sql
+++ supabase/repeatable/08042026_1226_BANKING_PAY_NEW.sql
@@ -34441,7 +34441,6 @@ BEGIN
 END;
 $function$;
 
-
 CREATE OR REPLACE FUNCTION public._pay_batch_item_economic_components(
   p_pay_batch_id uuid DEFAULT NULL,
   p_pay_batch_item_ids uuid[] DEFAULT NULL
@@ -34454,6 +34453,7 @@ RETURNS TABLE(
   key_type text,
   key_value text,
   source_amount_ex_vat numeric,
+  source_amount_inc_vat numeric,
   target_amount_ex_vat numeric,
   key_resolution_source text,
   key_resolution_failure_reason text
@@ -34485,6 +34485,7 @@ BEGIN
       pbi.segment_key AS item_segment_key,
       pbi.source_ref AS item_source_ref,
       pbi.amount_ex_vat AS item_amount_ex_vat,
+      pbi.amount_inc_vat AS item_amount_inc_vat,
       pbi.finance_case_id AS item_finance_case_id,
       pbi.finance_component_id AS item_finance_component_id,
       pbi.frozen_target_amount_ex_vat AS item_frozen_target_amount_ex_vat,
@@ -34549,6 +34550,7 @@ BEGIN
       bi.item_segment_key,
       bi.item_source_ref,
       bi.item_amount_ex_vat,
+      bi.item_amount_inc_vat,
       bi.item_finance_case_id,
       bi.item_finance_component_id,
       bi.item_frozen_target_amount_ex_vat,
@@ -34583,6 +34585,7 @@ BEGIN
       pi.item_segment_key,
       pi.item_source_ref,
       pi.item_amount_ex_vat,
+      pi.item_amount_inc_vat,
       pi.item_finance_case_id,
       pi.item_finance_component_id,
       pi.item_frozen_target_amount_ex_vat,
@@ -34768,6 +34771,7 @@ BEGIN
       rkc.item_type_raw,
       rkc.item_source_ref,
       rkc.item_amount_ex_vat,
+      rkc.item_amount_inc_vat,
       rkc.item_frozen_target_amount_ex_vat,
       rkc.item_frozen_source_amount,
       rkc.item_has_source_target_split,
@@ -34830,7 +34834,46 @@ BEGIN
         WHERE source_candidates.source_text_value IS NOT NULL
           AND btrim(source_candidates.source_text_value) ~ '^-?[0-9]+(\.[0-9]+)?$'
         LIMIT 1
-      ) AS raw_artifact_source_amount_ex_vat
+      ) AS raw_artifact_source_amount_ex_vat,
+      (
+        SELECT source_inc_candidates.source_inc_text_value::numeric
+        FROM (
+          VALUES
+            (rkc.item_frozen_source_basis_json->>'source_amount_inc_vat'),
+            (rkc.item_frozen_source_basis_json->>'source_pay_inc_vat'),
+            (rkc.item_frozen_source_basis_json->>'source_entitlement_amount_inc_vat'),
+            (rkc.item_frozen_source_basis_json->>'source_reservation_amount_inc_vat'),
+            (rkc.item_frozen_source_basis_json->>'pay_inc_vat'),
+            (rkc.item_frozen_source_basis_json->>'amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'source_amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'source_pay_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'source_entitlement_amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'source_reservation_amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'basis_source_amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json->>'reserved_source_amount_inc_vat'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,source_amount_inc_vat}'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,source_pay_inc_vat}'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,source_entitlement_amount_inc_vat}'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,source_reservation_amount_inc_vat}'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,pay_inc_vat}'),
+            (rkc.item_frozen_component_snapshot_json#>>'{source_basis_json,amount_inc_vat}'),
+            (rkc.item_frozen_resolution_payload_json->>'source_amount_inc_vat'),
+            (rkc.item_frozen_resolution_payload_json->>'source_pay_inc_vat'),
+            (rkc.item_frozen_resolution_payload_json->>'source_entitlement_amount_inc_vat'),
+            (rkc.item_frozen_resolution_payload_json->>'source_reservation_amount_inc_vat'),
+            (rkc.item_frozen_resolution_result_json->>'source_amount_inc_vat'),
+            (rkc.item_frozen_resolution_result_json->>'source_pay_inc_vat'),
+            (rkc.item_frozen_resolution_result_json->>'source_entitlement_amount_inc_vat'),
+            (rkc.item_frozen_resolution_result_json->>'source_reservation_amount_inc_vat'),
+            (rkc.item_single_breakdown_meta_json->>'source_amount_inc_vat'),
+            (rkc.item_single_breakdown_meta_json->>'source_pay_inc_vat'),
+            (rkc.item_single_breakdown_meta_json->>'source_entitlement_amount_inc_vat'),
+            (rkc.item_single_breakdown_meta_json->>'source_reservation_amount_inc_vat')
+        ) AS source_inc_candidates(source_inc_text_value)
+        WHERE source_inc_candidates.source_inc_text_value IS NOT NULL
+          AND btrim(source_inc_candidates.source_inc_text_value) ~ '^-?[0-9]+(\.[0-9]+)?$'
+        LIMIT 1
+      ) AS raw_artifact_source_amount_inc_vat
     FROM raw_key_candidates AS rkc
     LEFT JOIN segment_date_pick AS sdp
       ON sdp.batch_item_id = rkc.batch_item_id
@@ -34866,6 +34909,25 @@ BEGIN
           ELSE coalesce(ac.raw_artifact_source_amount_ex_vat, ac.raw_target_amount_ex_vat, ac.item_amount_ex_vat)
         END
       ), 2)::numeric AS final_source_amount_ex_vat,
+      round(abs(coalesce(
+        ac.raw_artifact_source_amount_inc_vat,
+        CASE
+          WHEN ac.item_has_source_target_split THEN NULL
+          ELSE ac.item_amount_inc_vat
+        END,
+        CASE
+          WHEN ac.item_type_norm IN ('SEGMENT_DELTA', 'EXPENSE_DELTA', 'ADJUSTMENT_DELTA', 'MILEAGE_DELTA')
+            THEN coalesce(
+              ac.entitlement_source_amount_ex_vat,
+              ac.raw_artifact_source_amount_ex_vat,
+              CASE
+                WHEN ac.item_has_source_target_split THEN NULL
+                ELSE coalesce(ac.raw_target_amount_ex_vat, ac.item_amount_ex_vat)
+              END
+            )
+          ELSE coalesce(ac.raw_artifact_source_amount_ex_vat, ac.raw_target_amount_ex_vat, ac.item_amount_ex_vat)
+        END
+      )), 2)::numeric AS final_source_amount_inc_vat,
       round(abs(coalesce(
         ac.raw_target_amount_ex_vat,
         ac.item_amount_ex_vat,
@@ -34981,6 +35043,7 @@ BEGIN
       rr.resolved_key_type,
       rr.resolved_key_value,
       rr.final_source_amount_ex_vat,
+      rr.final_source_amount_inc_vat,
       rr.final_target_amount_ex_vat,
       rr.resolved_key_source,
       CASE
@@ -35002,6 +35065,7 @@ BEGIN
     CASE WHEN fr.final_failure_reason IS NULL THEN fr.resolved_key_type ELSE NULL END AS key_type,
     CASE WHEN fr.final_failure_reason IS NULL THEN fr.resolved_key_value ELSE NULL END AS key_value,
     CASE WHEN fr.final_source_amount_ex_vat IS NULL THEN NULL ELSE round(fr.final_source_amount_ex_vat, 2)::numeric END AS source_amount_ex_vat,
+    CASE WHEN fr.final_source_amount_inc_vat IS NULL THEN NULL ELSE round(fr.final_source_amount_inc_vat, 2)::numeric END AS source_amount_inc_vat,
     CASE WHEN fr.final_target_amount_ex_vat IS NULL THEN NULL ELSE round(fr.final_target_amount_ex_vat, 2)::numeric END AS target_amount_ex_vat,
     CASE WHEN fr.final_failure_reason IS NULL THEN fr.resolved_key_source ELSE 'KEY_RESOLUTION_FAILED' END AS key_resolution_source,
     fr.final_failure_reason AS key_resolution_failure_reason
@@ -35009,6 +35073,7 @@ BEGIN
   ORDER BY fr.batch_id, fr.batch_item_id;
 END;
 $function$;
+
 
 
 CREATE OR REPLACE FUNCTION public._pay_batch_item_source_reservation_amount_ex_vat(p_pay_batch_item_id uuid)
