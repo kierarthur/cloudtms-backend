3e8cf0a5925141c0926035c404f04b1530731e30
kierarthur <kier@arthur-rai.co.uk>
2026-07-28 21:20:49 +0100

    Publish Invoice V8 NHSP generation authority

--- tests/invoice_async_backend.test.js
+++ tests/invoice_async_backend.test.js
@@ -41,7 +41,7 @@ import {
 } from '../invoice-document-processor/src/receipt-contract.js';
 
 const V8_ACTOR_ID = '00000000-0000-4000-8000-000000000010';
-const V8_FUNCTION_MANIFEST = '4270bc7ad6b9e0c57d95d26969cdbd88f8016db1f95b2d47f770f3b27bf842b3';
+const V8_FUNCTION_MANIFEST = 'f31d373f06bb3ad49df41aa158ba9707896294ef303b5cfd71b4e1748ed8d2d1';
 const V8_CURSOR_SECRET = 'test-session-secret-with-more-than-thirty-two-characters';
 
 function v8DatabaseContract(overrides = {}) {
